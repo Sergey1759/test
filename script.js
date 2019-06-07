faf65003ef0b96ff30bcae0a60d40332a924943b
Sergey1759 <lisichka1759@gmail.com>
2019-06-07 23:54:49 +0300

    txt to Object

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+let PerentObj = function(){
+    this.body = document.body;
+}
 function Request(string,callback){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
@@ -13,7 +15,7 @@ function Request(string,callback){
     request.send();
     
 }
-Request('exemple.txt',Main);
+Request('exempleIII.txt',Main);
 
 function Main(text){
     let json = ToJson(text);
@@ -31,32 +33,60 @@ function ToJson(text){
     } else if(~text.indexOf("-level-IV-")) {
         return WordsAndMeaning(text); 
     } // Expansion space
-    
     function OneOfAll(text){
-        
+        text = text.split('-q-');
+        text.splice(0, 1); //delete "-level-I-"
+        for(let i = 0;  i < text.length; i++){
+            let buf = { id : i, task : '', right : [], answers : []};           
+            text[i] = text[i].split('-v-');
+            buf.task = text[i][0];
+            for(let j = 0; j < text[i].length; j++){
+                if(!~text[i][j].indexOf("-t-") && j != 0){
+                    buf.answers.push({id : i*10+j+1000, value : text[i][j]});
+                }
+                    let que_ans = text[i][j].split('-t-');
+                        if(que_ans.length == 2){
+                            buf.right.push(i*10+j+1000);
+                            buf.answers.push({id : i*10+j+1000, value : que_ans[1]});
+                        }
+                }
+             text[i] = buf;
+        }
+        return text;
     }
     function SeveralOfAll(text){
-        
+        return OneOfAll(text);
     }
     function MissingWord(text){
-        
+        text = text.split('-q-');
+        text.splice(0, 1); //delete "-level-III-"
+        for(let i = 0;  i < text.length; i++){
+            let buf = { id : i, task : '', answer : ''};    
+            text[i] = text[i].split('-t-');
+            buf.task = text[i][0] + '......' + text[i][2];
+            buf.answer = text[i][1].split(' ');
+            buf.answer = buf.answer.join('');
+            text[i] = buf;
+        }
+        return text;
     }
     function WordsAndMeaning(text){
         text = text.split('-task-');
+        text.splice(0, 1); //delete "-level-IV-"
         for(let i = 0;  i < text.length; i++){
-            let buf = { id : i, task : '', question : [], answer : []};           
+            let buf = { id : i, task : '', question : [], answers : []};           
             text[i] = text[i].split('-que-');
             buf.task = text[i][0];
                 for(let j = 0; j < text[i].length; j++){
                     let que_ans = text[i][j].split('-ans-');
                         if(que_ans.length == 2){
                             buf.question.push({id : i*10+j+1000, value : que_ans[0]});
-                            buf.answer.push({id : i*10+j+1000, value : que_ans[1]});
+                            buf.answers.push({id : i*10+j+1000, value : que_ans[1]});
                         }
                 }
             text[i] = buf;
         }
         return text;
     }
-
+    
 }
