b1f1da1c413d91ed20d3e309920ad8396072fae8
Sergey1759 <lisichka1759@gmail.com>
2019-06-08 02:32:02 +0300

    function add

--- script.js
+++ script.js
@@ -12,39 +12,13 @@ let Obj = function(){
 let Elem = function(elem){
     this.elem = elem;
     let level = this.elem.level;
-    if(level == 1){
-        this.add = function add(){
-            let form = document.createElement('form');
-            form.innerHTML = `
-            
-  <p>${elem.task}</p>
-  <div>
-    <input type="radio" id="${elem.answers[0].id}"
-     name="contact" value="email">
-    <label for="${elem.answers[0].id}">${elem.answers[0].value}</label>
-
-    <input type="radio" id="contactChoice2"
-     name="contact" value="phone">
-    <label for="contactChoice2">Phone</label>
-
-    <input type="radio" id="contactChoice3"
-     name="contact" value="mail">
-    <label for="contactChoice3">Mail</label>
-  </div>
-  <div>
-    <button type="submit">Submit</button>
-  </div>
-</form>
-`;
-            document.body.appendChild(form);
-        }
-    }
-    if(level == 2){
-        this.add = function add(){
-            let div = document.createElement('div');
-            div.innerHTML = elem.task;
-            document.body.appendChild(div);
-        }
+    
+    if(level == 1 || level == 2){
+        this.add = add_1_2;
+    } else if (level == 3){
+        this.add = add_3;
+    }else if (level == 4){
+        this.add = add_4;
     }
     
 }
@@ -70,6 +44,7 @@ function Request(string,callback,kil){
 }
 
 four_level();
+
 function getArrrayObjects(text,kil){
     let json = ToJson(text);
     if(kil > json.length){ kil = json.length; 
@@ -79,14 +54,12 @@ function getArrrayObjects(text,kil){
         array.addElement(buf_arr[i]);
     }
     array.rand();
-    console.log(array.arr);
 }
 function four_level(){
     Request('exempleI.txt',getArrrayObjects,2);
     Request('exempleII.txt',getArrrayObjects,2);
-//    Request('exempleII.txt',getArrrayObjects,5);
-//    Request('exempleIII.txt',getArrrayObjects,5);
-//    Request('exempleIV.txt',getArrrayObjects,5);
+    Request('exempleIII.txt',getArrrayObjects,2);
+    Request('exempleIV.txt',getArrrayObjects,2);
 }
 
 
@@ -101,6 +74,7 @@ function ToJson(text){
     } else if(~text.indexOf("-level-IV-")) {
         return WordsAndMeaning(text); 
     } // Expansion space
+    
     function OneOfAll(text,level_II){
         text = text.split('-q-');
         let level = level_II || 1;//refactoring
@@ -124,9 +98,11 @@ function ToJson(text){
         }
         return text;
     }
+    
     function SeveralOfAll(text){
         return OneOfAll(text,2); 
     }
+    
     function MissingWord(text){
         text = text.split('-q-');
         let level = 3;//refactoring
@@ -142,6 +118,7 @@ function ToJson(text){
         }
         return text;
     }
+    
     function WordsAndMeaning(text){
         text = text.split('-task-');
         let level = 4; //refactoring
@@ -176,4 +153,56 @@ while( i < count){
   }
 }
 return mas1;
+}
+
+function add_1_2(){
+    let type = this.elem.level == 1 ? 'radio' : 'checkbox';
+    let form = document.createElement('form');
+    form.id = this.elem.id;
+    let label = '';
+    
+    for(let i = 0; i < this.elem.answers.length; i++){
+        label += `<input type="${type}" id="${this.elem.answers[i].id}"name="contact" value="email">
+                  <label for="${this.elem.answers[i].id}">${this.elem.answers[i].value}</label>`
+    }
+    
+    form.innerHTML = `
+        <p>${this.elem.task}</p>
+        ${label}
+        <div id = "${this.elem.id} name = "check">перевірити</div>`;
+    
+    document.body.appendChild(form);
+}
+function add_3(){
+    let div = document.createElement('div');
+    div.id = this.elem.id;
+    div.innerHTML = ` <p>${this.elem.task}</p> <input type="text" ></input>`;
+    document.body.appendChild(div);
+}
+function add_4(){
+    let div = document.createElement('div');
+    div.id = this.elem.id;
+    div.classList.add('select-box');
+    let option = '';
+    let all_select = '';
+    let start_select_question = "<div class=select-question>";
+    let end_select_question = "</div>";
+    
+    for(let j = 0; j < this.elem.answers.length; j++){
+        option += `<option value="${this.elem.answers[j].id}">${this.elem.answers[j].value}</option> `
+    }
+    
+    for(let i = 0; i < this.elem.question.length; i++){
+        let p = `<p class="text "value="${this.elem.question[i].id}">${this.elem.question[i].value}</p> `;
+            all_select += `
+            ${start_select_question} 
+            ${p}
+            <select class="select-answer" name="select"> 
+            ${option}
+            </select>
+            ${end_select_question}`
+    }
+    
+    div.innerHTML = all_select;
+    document.body.appendChild(div);
 }
